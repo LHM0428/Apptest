013d1c39b3456f741d347c66d55a42e860f6b527
LHM0428 <lhm3031@hanmail.net>
2016-08-09 14:51:00 +0900

    2 commit - cover fragment 추가 - main fragment 시작

--- app/src/main/java/com/apptest/lee/apptest/MainActivity.java
+++ app/src/main/java/com/apptest/lee/apptest/MainActivity.java
@@ -1,13 +1,48 @@
 package com.apptest.lee.apptest;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    final String TAG = "MainActivity";
+    int mCurrentFragmentIdx; // 현재 Fragment Index를 받는 인수;
+    public final static int FRAGMENT_COVER = 0; // COVER Fragment 상수 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCurrentFragmentIdx = FRAGMENT_COVER;
+        fragmentReplace(mCurrentFragmentIdx);
     }
+    // fragment를 바꾸기 위한 메소드
+    public void fragmentReplace(int replaceFragmentIdx){
+        Fragment newFragment = null;
+        newFragment = getFragment(replaceFragmentIdx);
+
+        Log.d(TAG, "fragment Replace : "+ replaceFragmentIdx);
+
+        //fragment를 바꾼다.
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, newFragment);
+        transaction.commit();
+    }
+    // fragment 인덱스를 fragment를 통해 받아오기 위한 메소드
+    private Fragment getFragment(int idx) {
+        Fragment newFragment = null;
+
+        switch (idx){
+            case FRAGMENT_COVER :
+                newFragment = new CoverFragment();
+                break;
+        }
+        return  newFragment;
+    }
+
 }
